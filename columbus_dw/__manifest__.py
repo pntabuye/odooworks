6bb697b8384902359b93a9e6e0089cd8abd741f7
Paul Ntabuye Butera <pntabuye@gmail.com>
2020-05-24 15:57:53 +0200

    [ADD] init CDW module

---
+++ columbus_dw/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "columbus_dw",
+    'summary': """Columbus DW connector for Odoow""",
+    'version': '13.0.0.0.1',
+    'category': 'Document Management',
+    "license": "LGPL-3",
+    'website': "http://www.planbweb.com",
+    'author': "Paul Ntabuye Butera",
+    "contributors": [
+        "Paul Ntabuye Butera <pntabuye@planbweb.com>",
+    ],
+    'depends': [
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    'description': """
+        This connector allows to connect Columbus DW from Macro4 with Odoo
+    """,
+    "images": [
+        'static/description/banner.png'
+    ],
+    'demo': [
+    ],
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "application": True,
+    "installable": True,
+}
